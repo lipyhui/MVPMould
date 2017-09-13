19da427e16397717ae6cd9ade4a4717bc998a697
liph <penghui.li@kawakp.com>
2017-09-13 23:14:02 +0800

    支持databinding、lambda、rxjava、kotlin等

--- demo/src/main/java/com/kawakp/kp/application/MainActivity.java
+++ demo/src/main/java/com/kawakp/kp/application/MainActivity.java
@@ -1,9 +1,10 @@
 package com.kawakp.kp.application;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
 
+import com.kawakp.kp.application.databinding.ActivityMainBinding;
 import com.kawakp.kp.kernel.KernelJNI;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +12,12 @@ public class MainActivity extends AppCompatActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+//		setContentView(R.layout.activity_main);
+
+		ActivityMainBinding mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
 		// Example of a call to a native method
-		TextView tv = (TextView) findViewById(R.id.sample_text);
-		tv.setText(KernelJNI.stringFromJNI());
+//		TextView tv = (TextView) findViewById(R.id.sample_text);
+		mBinding.testText.setText(KernelJNI.stringFromJNI());
 	}
 }
