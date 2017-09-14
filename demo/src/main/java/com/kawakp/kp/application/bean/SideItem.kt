2ea705fc223d4dd91e51d70f997a2eb0f8b2ed47
liph <penghui.li@kawakp.com>
2017-09-14 21:24:53 +0800

    修改侧边栏为自动匹配

---
+++ demo/src/main/java/com/kawakp/kp/application/bean/SideItem.kt
@@ -0,0 +1,12 @@
+package com.kawakp.kp.application.bean
+
+/**
+ * 创建人: penghui.li
+ * 创建时间: 2017/9/14
+ * 修改人:penghui.li
+ * 修改时间:2017/9/14
+ * 修改内容:
+ *
+ * 功能描述:侧边栏数据项
+ */
+data class SideItem(val name: String)
