f260f557f228864f84eef991ca9820e21846ddef
liph <penghui.li@kawakp.com>
2017-09-18 22:14:51 +0800

    修改内核base修改代码，添加fragment的创建、显示生命周期

--- demo/src/main/java/com/kawakp/kp/application/MainActivity.java
+++ demo/src/main/java/com/kawakp/kp/application/MainActivity.java
@@ -1,20 +1,14 @@
 package com.kawakp.kp.application;
 
-import android.databinding.DataBindingUtil;
-import android.os.Bundle;
-
 import com.kawakp.kp.application.databinding.ActivityMainBinding;
 import com.kawakp.kp.kernel.KernelJNI;
 import com.kawakp.kp.kernel.base.BaseBindingActivity;
 
-import org.jetbrains.annotations.NotNull;
-
 public class MainActivity extends BaseBindingActivity<ActivityMainBinding> {
 
-	@NotNull
 	@Override
-	public ActivityMainBinding createDataBinding(Bundle savedInstanceState) {
-		return DataBindingUtil.setContentView(this, R.layout.activity_main);
+	public int getLayoutId() {
+		return R.layout.activity_main;
 	}
 
 	@Override
--- kernel/src/main/java/com/kawakp/kp/kernel/base/BaseBindingFragment.kt
+++ kernel/src/main/java/com/kawakp/kp/kernel/base/BaseBindingFragment.kt
@@ -1,11 +1,11 @@
 package com.kawakp.kp.kernel.base
 
+import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.trello.rxlifecycle2.components.support.RxFragment
 
 /**
  * 创建人: penghui.li
@@ -16,13 +16,12 @@ import com.trello.rxlifecycle2.components.support.RxFragment
  *
  * 功能描述:该基类进行Fragment的View绑定，所有Fragment继承该类。
  */
-abstract class BaseBingingFragment<B: ViewDataBinding> : RxFragment(){
+abstract class BaseBingingFragment<B: ViewDataBinding> : BaseLazyFragment(){
 
     lateinit var mBinding : B
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        mBinding = createDataBinding(inflater,container,savedInstanceState)
-        initView()
+        mBinding = DataBindingUtil.inflate(inflater, getLayoutId(), container, false)
         return mBinding.root
 
     }
@@ -31,8 +30,6 @@ abstract class BaseBingingFragment<B: ViewDataBinding> : RxFragment(){
         super.onViewCreated(view, savedInstanceState)
     }
 
-    abstract fun  createDataBinding(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): B
-
-    abstract fun initView()
+    abstract fun getLayoutId(): Int
 
 }
