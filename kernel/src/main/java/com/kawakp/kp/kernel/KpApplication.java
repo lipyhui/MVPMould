cbccd25496cdac8fa416797afeb4e779f50cd5af
liph <penghui.li@kawakp.com>
2017-10-06 14:48:34 +0800

    添加数据库支持

--- kernel/src/main/java/com/kawakp/kp/kernel/KpApplication.java
+++ kernel/src/main/java/com/kawakp/kp/kernel/KpApplication.java
@@ -3,6 +3,9 @@ package com.kawakp.kp.kernel;
 import android.app.Application;
 import android.content.Context;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * 创建人: penghui.li
  * 创建时间: 2017/9/15
@@ -17,6 +20,8 @@ public class KpApplication extends Application {
 	private static KpApplication mContext;
 	/* 配置屏幕常亮*/
 	private static boolean ScreenOn = true;
+	/*获取数据库*/
+	private static Realm REALM_INSTANCE;
 
 	@Override
 	public void onCreate()
@@ -24,6 +29,9 @@ public class KpApplication extends Application {
 		super.onCreate();
 
 		mContext = this;
+
+		//初始化realm数据库
+		REALM_INSTANCE = initRealm();
 	}
 
 	/**
@@ -46,9 +54,32 @@ public class KpApplication extends Application {
 
 	/**
 	 * 设置屏幕是否常亮
+	 *
 	 * @param screenOn
 	 */
 	public static void setScreenOn(boolean screenOn) {
 		ScreenOn = screenOn;
+	}
+
+	/**
+	 * 获取Realm数据库单列
+	 *
+	 * @return
+	 */
+	public static Realm getRealmInstance() {
+		return REALM_INSTANCE;
+	}
+
+	/**
+	 * 	初始化realm数据库
+	 */
+	protected Realm initRealm(){
+		Realm.init(this);
+		RealmConfiguration config = new RealmConfiguration.Builder()
+				.schemaVersion(1)
+				.name("KAWAKP.realm")
+				.deleteRealmIfMigrationNeeded()
+				.build();
+		return Realm.getInstance(config);
 	}
 }
