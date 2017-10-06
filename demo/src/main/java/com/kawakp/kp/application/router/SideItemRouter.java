ec40c375310e42100b5b752e814089ec884dc3f0
liph <penghui.li@kawakp.com>
2017-10-06 15:25:57 +0800

    修复齐圣提交侧边栏冲突

--- demo/src/main/java/com/kawakp/kp/application/router/SideItemRouter.java
+++ demo/src/main/java/com/kawakp/kp/application/router/SideItemRouter.java
@@ -13,31 +13,11 @@ import com.kawakp.kp.application.ui.fragment.main.MainFragment;
  * 修改人:penghui.li
  * 修改时间:2017/9/14
  * 修改内容:
- * <p>
+ *
  * 功能描述:侧边栏fragment功能列表配置
  */
 
 public enum SideItemRouter {
-<<<<<<< HEAD
-    CHART(1, "折线图"),
-    ANIM(2, "动画");
-
-    private int mType = 1;
-    private String mSideName = "折线图";
-
-    SideItemRouter(int type, String name) {
-        mSideName = name;
-        mType = type;
-    }
-
-    public String getSideName() {
-        return mSideName;
-    }
-
-    public Fragment getFragment() {
-        return MainFragment.Companion.newInstance(mType);
-    }
-=======
 	CHART(1, R.string.side_char_img, R.string.side_char),
 	ANIM(2, R.string.side_anim_img, R.string.side_anim);
 
@@ -62,5 +42,4 @@ public enum SideItemRouter {
 	public String getSideName() {
 		return mSideName;
 	}
->>>>>>> 09d532aaaf49234f5f8e4e780f33402123c37b26
 }
