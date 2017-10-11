fe34aa017efc7fc70f83f0279cc9559c7f19a493
liph <penghui.li@kawakp.com>
2017-10-11 10:20:20 +0800

    修改数据表取消位置信息存入数据库

--- demo/src/main/java/com/kawakp/kp/application/bean/FormItem.kt
+++ demo/src/main/java/com/kawakp/kp/application/bean/FormItem.kt
@@ -1,6 +1,7 @@
 package com.kawakp.kp.application.bean
 
 import io.realm.RealmObject
+import io.realm.annotations.Ignore
 import io.realm.annotations.PrimaryKey
 
 /**
@@ -13,6 +14,7 @@ import io.realm.annotations.PrimaryKey
  * 功能描述:表格列表数据项。注意所有变量都需要初始化默认值，就算是null也需要初始化
  */
 open class FormItem(
+        @Ignore
         open var bg: Boolean = false,
         @PrimaryKey open var time: String = "2017-04-07 03:00:00",
         open var oneP: String = "0",
