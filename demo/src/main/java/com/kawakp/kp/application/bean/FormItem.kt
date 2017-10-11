c565cadaea5f5dc97ee8e85f60bf33225cee53c5
liph <penghui.li@kawakp.com>
2017-10-11 10:49:22 +0800

    修改数据表类排版

--- demo/src/main/java/com/kawakp/kp/application/bean/FormItem.kt
+++ demo/src/main/java/com/kawakp/kp/application/bean/FormItem.kt
@@ -14,8 +14,7 @@ import io.realm.annotations.PrimaryKey
  * 功能描述:表格列表数据项。注意所有变量都需要初始化默认值，就算是null也需要初始化
  */
 open class FormItem(
-        @Ignore
-        open var bg: Boolean = false,
+        @Ignore open var bg: Boolean = false,
         @PrimaryKey open var time: String = "2017-04-07 03:00:00",
         open var oneP: String = "0",
         open var twoP: String = "0",
