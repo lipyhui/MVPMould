b729aea02ce4e25eb229e802c6a3c11165a2f474
liph <penghui.li@kawakp.com>
2017-10-26 13:51:34 +0800

    修改socket返回bool类型不同步的问题

--- kernel/src/main/java/com/kawakp/kp/kernel/plc/PLCRespElement.kt
+++ kernel/src/main/java/com/kawakp/kp/kernel/plc/PLCRespElement.kt
@@ -8,12 +8,12 @@ package com.kawakp.kp.kernel.plc
  * 修改内容:
  *
  * 功能描述:单元件 PLC 读写响应数据
- *          bit: 存放 PLC BOOL类型数据,默认为 false
+ *          bool: 存放 PLC BOOL类型数据,默认为 false
  *          word: 存放 PLC WORD类型数据,默认为 0
  *          dword: 存放 PLC DWORD类型数据,默认为 0
  *          real: 存放 PLC REAL类型数据,默认为 0
  */
-data class PLCRespElement constructor(val bit: Boolean = false,
+data class PLCRespElement constructor(val bool: Boolean = false,
                                       val word: Int = 0,
                                       val dword: Int = 0,
                                       val real: Float = 0F)
