4427c980b1ba6b0182f5d9da4e3d9d50609c6eb2
liph <penghui.li@kawakp.com>
2018-01-12 13:22:12 +0800

    添加注释

--- demo/src/main/java/com/kawakp/kp/application/ui/activity/main/MainActivity.java
+++ demo/src/main/java/com/kawakp/kp/application/ui/activity/main/MainActivity.java
@@ -4,6 +4,15 @@ import com.kawakp.kp.application.R;
 import com.kawakp.kp.application.base.BaseActivity;
 import com.kawakp.kp.application.databinding.ActivityMainBinding;
 
+/**
+ * 创建人: penghui.li
+ * 创建时间: 2017/9/14
+ * 修改人:penghui.li
+ * 修改时间:2017/9/14
+ * 修改内容:
+ *
+ * 功能描述:主界面
+ */
 public class MainActivity extends BaseActivity<MainPresenter, ActivityMainBinding> implements MainAble {
 
 	@Override
