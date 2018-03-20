352eedfdd877063c5fef5cb37adaf37541efdeb1
liph <penghui.li@kawakp.com>
2018-03-20 13:29:12 +0800

    修改 PLC 读写为 plcd APP 读写方式，修复 X、Y 元件读写错误的问题

--- kernel/src/main/java/com/kawakp/kp/kernel/plc/kawa/SocketClient.java
+++ kernel/src/main/java/com/kawakp/kp/kernel/plc/kawa/SocketClient.java
@@ -32,13 +32,12 @@ public class SocketClient {
 	public static byte[] sendMsg(final byte[] data) {
 		//创建socket
 		LocalSocket client = new LocalSocket();
-		LocalSocketAddress address = new LocalSocketAddress(SOCKET_NAME, LocalSocketAddress.Namespace
-				.RESERVED);
+		LocalSocketAddress address = new LocalSocketAddress(SOCKET_NAME);
 
 		//连接本地socket进程
 		boolean isConnected = false;
 		int connectCount = 0;
-//					Log.e("socket_Test", "start connect!");
+//					Log.e("java_plcd_SocketClient", "start connect !!!!!!!!!!!");
 		while (!isConnected && connectCount < 10) {
 			try {
 				if (connectCount != 0) {
@@ -49,7 +48,7 @@ public class SocketClient {
 			} catch (Exception e) {
 				connectCount++;
 				isConnected = false;
-				Log.e("SocketClient", "Connect fail, err = " + e.toString());
+				Log.e("java_plcd_SocketClient", "Connect fail, err = " + e.toString());
 			}
 		}
 
@@ -65,27 +64,42 @@ public class SocketClient {
 			client.setSoTimeout(TIMEOUT);
 
 			//发送数据
-//					Log.e("socket_Test", "start write!");
+//					Log.e("java_plcd_SocketClient", "start write! data lenght = " + data.length);
 			if (data.length > 0) {
 				client.getOutputStream().write(data);
+				client.shutdownOutput();
 			}
 
 			//接收响应
-//					Log.e("socket_Test", "start read!");
+//					Log.e("java_plcd_SocketClient", "start read!");
 			byte[] result = readStream(client.getInputStream());
+
+//			Log.e("java_plcd_SocketClient", "READ data result length = " + result.length);
 
 			//关闭输入流、输出流、socket
 		/*	client.shutdownOutput();
 			client.shutdownInput();*/
 			client.close();
 
+//			Log.e("java_plcd_SocketClient", "READ WRITE END !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
 			return result;
 		} catch (Exception e) {
 			//socket读写操作失败
-			Log.e("SocketClient", "SendMsg fail, err = " + e.toString());
+			Log.e("java_plcd_SocketClient", "SendMsg fail, err = " + e.toString());
 			return new byte[0];
 		}
 	}
+
+	private static final byte READ_CODE = 0X69;    //读 PLC 数据功能码
+	private static final byte WRITE_CODE = 0X68;    //写 PLC 数据功能码
+
+	private static final byte DATA_MIN_LENGTH = 5;    //响应数据最小长度
+
+	private static final byte READ_DATA_HEAD_LENGTH = 5;    //读响应数据最小长度，从站地址+功能码+子功能码+数据域长度
+	private static final byte CRC_LENGTH = 2;    //接收数据 CRC 校验位占用字节数
+
+	private static final byte WRITE_RESPOND_LENGTH = 5;    //写响应数据长度
 
 	/**
 	 * 读取数据
@@ -96,24 +110,44 @@ public class SocketClient {
 	 */
 	private static byte[] readStream(InputStream inStream) throws Exception {
 		ByteArrayOutputStream outSteam = new ByteArrayOutputStream();
-		byte[] buffer = new byte[1024];
+		byte[] buffer = new byte[2048];
 		int len = -1;
 		while ((len = inStream.read(buffer)) != -1) {
 			outSteam.write(buffer, 0, len);
-		}
-		outSteam.close();
-		inStream.close();
-		return outSteam.toByteArray();
+			byte[] bytes = outSteam.toByteArray();
 
-		/*int count = 0;
-		while (count == 0) {
-			count = inStream.available();
+//			Log.e("java_plcd_SocketClient", "byte length = " + bytes.length);
+//			for (byte b : bytes){
+//				Log.e("java_plcd_SocketClient", "byte = " + Integer.toHexString(b & 0XFF));
+//			}
+
+			if (bytes.length < DATA_MIN_LENGTH) {
+				continue;
+			}
+
+			switch (bytes[1]) {
+				case READ_CODE: {
+					int dataLength = (bytes[3] << 8) + bytes[4];
+//					Log.e("java_plcd_SocketClient", "read data length = " + dataLength);
+					if (bytes.length == (READ_DATA_HEAD_LENGTH + dataLength + CRC_LENGTH)) {	//接收到读的完整数据
+						return bytes;
+					}
+					continue;
+				}
+
+				case WRITE_CODE: {
+					if (bytes.length == WRITE_RESPOND_LENGTH) {	//接收到写响应的完整数据
+						return bytes;
+					}
+					continue;
+				}
+
+				default:	//不支持类型
+					return new byte[0];
+			}
 		}
-		byte[] bytes = new byte[count];
-		int readCount = 0; // 已经成功读取的字节的个数
-		while (readCount < count) {
-			readCount += inStream.read(bytes, readCount, count - readCount);
-		}
-		return bytes;*/
+
+		//正常情况运行不到这里
+		return new byte[0];
 	}
 }
